b4558b5988f997fa2d858577a5afb92f5c1a78ef
AIwinski <artur.iwinski@gmail.com>
2018-11-16 18:44:14 +0100

    refactor

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,42 +1,59 @@
-require('jquery');
-require('jquery-ui-bundle');
+import $ from 'jquery';
+import 'jquery-ui/ui/widgets/slider';
+
+let config = {
+    DATA_MIN: 'min',
+    DATA_MINDEFAULT: 'mindefault',
+    DATA_MAX: 'max',
+    DATA_MAXDEFAULT: 'maxdefault',
+    DATA_STEP: 'step',
+    VALUE_MIN: '[data-value-min]',
+    VALUE_MAX: '[data-value-max]',
+    DATA_SLIDER: 'slider',
+    SLIDER: '[data-slider]'
+}
 
 class RangeSlider {
     constructor(selector) {
         this.selector = selector;
-        this.sliderSelector = $(selector);
-        this.min = this.sliderSelector.data("min");
-        this.max = this.sliderSelector.data("max");
-        this.mindefault = this.sliderSelector.data("mindefault");
-        this.maxdefault = this.sliderSelector.data("maxdefault");
-        this.step = this.sliderSelector.data("step");
+        this.$slider = $(selector + ' > .slider');
+        this.$sliderContainer = $(selector);
+        this.min = this.$sliderContainer.data(config.DATA_MIN);
+        this.max = this.$sliderContainer.data(config.DATA_MAX);
+        this.mindefault = this.$sliderContainer.data(config.DATA_MINDEFAULT);
+        this.maxdefault = this.$sliderContainer.data(config.DATA_MAXDEFAULT);
+        this.step = this.$sliderContainer.data(config.DATA_STEP);
 
         this.init();
     }
 
+    getMinValue(){
+        return this.$slider.slider('values', 0);
+    }
+
+    getMaxValue(){
+        return this.$slider.slider('values', 1);
+    }
+
     init() {
-        console.log(this.selector)
-        let s = this.selector;
-        this.sliderSelector.slider({
+        this.$slider.slider({
             range: true,
             min: this.min,
             max: this.max,
             values: [this.mindefault, this.maxdefault],
             step: this.step,
-            slide: function (event, ui) {
-                $(s).find('[data-value-min]').text(ui.values[0]);
-                $(s).find('[data-value-max]').text(ui.values[1]);
+            slide: (event, ui) => {
+                this.$sliderContainer.find(config.VALUE_MIN).text(ui.values[0]);
+                this.$sliderContainer.find(config.VALUE_MAX).text(ui.values[1]);
             }
         });
-        $(this.selector).find('[data-value-min]').text($(this.selector).slider("values", 0));
-        $(this.selector).find('[data-value-max]').text($(this.selector).slider("values", 1));
+        
+        this.$sliderContainer.find(config.VALUE_MIN).text(this.$slider.slider("values", 0));
+        this.$sliderContainer.find(config.VALUE_MAX).text(this.$slider.slider("values", 1));
     }
 }
 
-
-new RangeSlider('[data-slider="price"]');
-new RangeSlider('[data-slider="powierzchnia"]');
-new RangeSlider('[data-slider="pomieszczenia"]');
-// new RangeSlider(190000, 350000, 190000, 220000, '.slider-container:nth-child(1) > .slider', '.slider-container:nth-child(2) > ', '.slider-container:nth-child(2) > .slider__value-label > .slider__value-label--max', 10000);
-// new RangeSlider(1, 4, 1, 3, '.slider-container:nth-child(3) > .slider', '.slider-container:nth-child(3) > .slider__value-label > .slider__value-label--min', '.slider-container:nth-child(3) > .slider__value-label > .slider__value-label--max', 1);
+var priceSlider = new RangeSlider('[data-slider="price"]');
+let areaSlider = new RangeSlider('[data-slider="powierzchnia"]');
+let roomsSlider = new RangeSlider('[data-slider="pomieszczenia"]');
 
